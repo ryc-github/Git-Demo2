92a2d2d21e6372bee6d0ecbd49702c9ce3fd667d
ryc <2579758528@qq.com>
2021-05-26 22:29:38 +0800

    git first commit

---
+++ src/main/java/com/ntlg/git/git.java
@@ -0,0 +1,13 @@
+package com.ntlg.git;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class git {
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String format = simpleDateFormat.format(date);
+        System.out.println(format);
+    }
+}
